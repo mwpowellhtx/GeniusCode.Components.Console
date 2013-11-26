f72efd942aaebb0081af2763d549f81cea0bb2d2
Michael Powell <mwpowellhtx@gmail.com>
2013-11-26 15:11:49 -0600

    making it nuget ready

--- src/NDesk.Options.Extensions/Properties/AssemblyInfo.cs
+++ src/NDesk.Options.Extensions/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9714d2d8-bc7b-4ca0-a9a0-a52a3bafd08a")]
+[assembly: Guid("7d0e08d7-b17d-44b3-9925-df67e912d832")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.*")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.*")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
